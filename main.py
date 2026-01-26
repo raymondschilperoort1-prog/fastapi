a8b3a147448b60727ba0ffa77055938cebe11151
raymondschilperoort1-prog <raymond.schilperoort1@gmail.com>
2026-01-26 12:51:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(
+    title="Accountant API",
+    version="1.0.2"
+)
 
 
 # -----------------------------
